b6e4820f0fcce3b2299a6f7eda6f5be2617b6044
aslee04 <mukhammadiev05@gmail.com>
2024-08-08 17:15:30 +0500

    61 group routes

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from 'next'
+import Link from 'next/link'
+import { Noto_Sans } from 'next/font/google'
+import { ReactNode } from 'react'
+
+import '@/assets/scss/global.scss'
+
+const notoSans = Noto_Sans({
+	display: 'swap',
+	subsets: ['latin', 'cyrillic'],
+	weight: ['300', '400', '500', '700']
+})
+
+export const metadata: Metadata = {
+	title: 'Authorization'
+}
+
+export default function AuthLayout({
+	children
+}: Readonly<{
+	children: ReactNode
+}>) {
+	return (
+		<html lang='ru'>
+			<body className={notoSans.className}>
+			  <main
+					className='tw-flex tw-flex-col tw-justify-between tw-items-center tw-p-24 tw-min-h-screen'
+				>
+					<h2>Authorization</h2>
+					{children}
+			  </main>
+			</body>
+		</html>
+	)
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+	title: 'NotFound'
+}
+
+export default function NotFound() {
+	return (
+		<div>
+			Page NotFound 404
+		</div>
+	)
+}
